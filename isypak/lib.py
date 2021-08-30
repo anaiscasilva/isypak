03c797191c2cbe44f104450eba44c25c9c1c1917
anaisa silva <anaisacsilva@gmail.com>
2021-08-30 16:18:32 +0100

    lib

---
+++ isypak/lib.py
@@ -0,0 +1,7 @@
+def try_me(side):
+    """This function return the volume and surface_area of a cube!'"""
+    volume = side **3
+    surface_area = 6 * (side**2)
+    return volume, surface_area
+
+    
